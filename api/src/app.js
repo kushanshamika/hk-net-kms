15cb36d7fb81c5e3ce32d17328686c846a58a0ea
Kushan Shamika <shamikakushan@gmail.com>
2024-01-14 06:34:18 +0530

    Enable CORS in API

--- api/src/app.js
+++ api/src/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
-mongoose.connect('mongodb+srv://hkDB:6ISc5s3753991SOm@cluster0.c6jw7zz.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://hkDB:6ISc5s3753991SOm@cluster0.c6jw7zz.mongodb.net/?retryWrites=true&w=majority');
 
 const authRoutes = require('./routes/authRoutes');
 const documentRoutes = require('./routes/documentRoutes');
