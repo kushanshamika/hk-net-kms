343a5e58c2e58ae253b965bf27727bdb141600b4
Kushan Shamika <shamikakushan@gmail.com>
2024-01-18 00:58:58 +0530

    Change Server URL

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://hk-net-dqz9i.ondigitalocean.app/',
 });
 
 export default instance;
